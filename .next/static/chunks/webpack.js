dee020b15a4c69825a8a2a39d15d4b0ca16943c9
이로움 <roum02@jobkorea.co.kr>
2022-11-12 23:28:04 +0900

    Add main layout [main__style]

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "2c8a92cd2c0fab42"; }
+/******/ 		__webpack_require__.h = function() { return "5354bdea0bcb132f"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/main/index.tsx
+++ pages/main/index.tsx
@@ -1,6 +1,22 @@
 const Main = () => {
     return (
-        <div>this is main</div>
+        <header>
+            <h1>2022 점심스터디 발제자 뽑기</h1>
+            <main>
+                <h2>인원:</h2>
+                <button type="button">
+                    뽑기
+                </button>
+            </main>
+            <footer>
+                <button type="button">
+                    인원 추가하기
+                </button>
+                <button type="button">
+                    뽑기 인원 변경하기
+                </button>
+            </footer>
+        </header>
     )
 }
 
